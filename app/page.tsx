60bcf36fed639961761c3cbd2f14c16702c4e119
Russell Frankpitt <75209707+rustyff12@users.noreply.github.com>
2025-12-19 00:29:11 +0100

    Projects layout. Changes to images

--- app/page.tsx
+++ app/page.tsx
@@ -3,13 +3,15 @@ import Projects from "@/components/Projects/Projects";
 import Skills from "@/components/Skills/Skills";
 
 import { heroData, socialsData, btnData } from "@/content/hero";
+import { projectsData } from "@/content/projects";
 import { skillsData } from "@/content/skills";
+
 export default function Home() {
   return (
     <>
       <Hero {...heroData} socials={socialsData} btn={btnData} />
       <Skills skills={skillsData} />
-      <Projects />
+      <Projects projects={projectsData} btn={btnData} />
     </>
   );
 }
--- components/Projects/Projects.tsx
+++ components/Projects/Projects.tsx
@@ -1,19 +1,57 @@
 import type { StaticImageData } from "next/image";
-
+import Button from "@/components/ui/Button";
+import { ButtonData } from "@/lib/types";
+import "./Projects.css";
+import Image from "next/image";
 export type Project = {
   title: string;
-  img: {
-    small: StaticImageData;
-    large: StaticImageData;
-    alt: string;
-  };
+  img: StaticImageData;
+
+  alt: string;
   tags: string[];
 };
 
-export default function Projects() {
+export default function Projects({
+  projects,
+  btn,
+}: {
+  projects: Project[];
+  btn: ButtonData;
+}) {
   return (
-    <>
-      <h2>Projects goes here</h2>
-    </>
+    <section className="projects-wrapper">
+      <div className="projects-header">
+        <h2 className="heading-xl">Projects</h2>
+        <Button name={btn.name} link={btn.link} />
+      </div>
+      <div className="cards-section">
+        {projects.map((item, index) => (
+          <article className="card" key={index}>
+            <div className="card-image">
+              <Image
+                src={item.img}
+                alt={item.alt}
+                width={342}
+                height={255}
+                sizes="342px"
+                className="card-image"
+              />
+            </div>
+            <div className="card-info">
+              <h3>{item.title}</h3>
+              <p className="card-tags">
+                {item.tags.map((item, index) => (
+                  <span key={index}>{item}</span>
+                ))}
+              </p>
+            </div>
+            <div className="card-links">
+              <Button name="View Project" link="https://www.netlify.com" />
+              <Button name="View Code" link="https://www.github.com" />
+            </div>
+          </article>
+        ))}
+      </div>
+    </section>
   );
 }
