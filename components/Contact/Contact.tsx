5da1b5818759ec0a665cc15ca22ec1a368846a70
Russell Frankpitt <75209707+rustyff12@users.noreply.github.com>
2026-01-19 22:12:16 +0100

    Contact and footer layouts

--- components/Contact/Contact.tsx
+++ components/Contact/Contact.tsx
@@ -3,53 +3,71 @@ import Break from "@/components/ui/Break";
 import Button from "@/components/ui/Button";
 import { socialsData } from "@/content/contact";
 import "./Contact.css";
+
 export default function Contact() {
   return (
     <div className="bottom-section">
-      <section id="contact">
-        <div className="contact-text">
-          <h3 className="contact-header">Contact</h3>
-          <p>
-            I would love to hear about your project and how I could help. Please
-            fill in the form, and I&apos;ll get back to you as soon as possible.
-          </p>
-        </div>
-        <form className="form-container">
-          <input id="name" type="text" placeholder="name" />
+      <div className="bottom-wrapper">
+        <section id="contact">
+          <div className="contact-text">
+            <h3 className="heading-xl">Contact</h3>
+            <p>
+              I would love to hear about your project and how I could help.
+              Please fill in the form, and I&apos;ll get back to you as soon as
+              possible.
+            </p>
+          </div>
+          <form className="form-container">
+            <div className="input-container">
+              <input id="name" type="text" placeholder="name" />
+              <span></span>
+            </div>
 
-          <input id="email" type="email" placeholder="name" />
+            <div className="input-container">
+              <input id="email" type="email" placeholder="email" />
+              <span></span>
+            </div>
 
-          <textarea name="message" id="message"></textarea>
-          <Button link="#" name="sned message" />
-        </form>
+            <div className="input-container">
+              <textarea
+                name="message"
+                id="message"
+                placeholder="message"
+              ></textarea>
+              <span></span>
+            </div>
+
+            <Button link="#" name="sned message" />
+          </form>
+        </section>
         <Break hideLarge={false} />
-      </section>
-      <footer>
-        <h4>adamkeys</h4>
-        <nav className="footer-nav-container">
-          <ul>
-            {socialsData.map((social) => (
-              <li key={social.name}>
-                <a
-                  href={social.link}
-                  target="_blank"
-                  rel="noopener noreferrer"
-                  className="social-link"
-                  aria-label={`Visit my ${social.name} profile`}
-                >
-                  <Image
-                    src={social.image}
-                    width={20}
-                    height={20}
-                    alt=""
-                    className="social-icon"
-                  />
-                </a>
-              </li>
-            ))}
-          </ul>
-        </nav>
-      </footer>
+        <footer>
+          <h4>adamkeys</h4>
+          <nav className="footer-nav-container">
+            <ul>
+              {socialsData.map((social) => (
+                <li key={social.name}>
+                  <a
+                    href={social.link}
+                    target="_blank"
+                    rel="noopener noreferrer"
+                    className="social-link"
+                    aria-label={`Visit my ${social.name} profile`}
+                  >
+                    <Image
+                      src={social.image}
+                      width={20}
+                      height={20}
+                      alt=""
+                      className="social-icon"
+                    />
+                  </a>
+                </li>
+              ))}
+            </ul>
+          </nav>
+        </footer>
+      </div>
     </div>
   );
 }
