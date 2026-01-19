489026cecc991220471485195260fa47829aef1c
Russell Frankpitt <75209707+rustyff12@users.noreply.github.com>
2026-01-19 19:16:23 +0100

    Mobile layout bottom section

---
+++ content/contact.ts
@@ -0,0 +1,15 @@
+import github from "@/public/images/icon-github.svg";
+import frontend from "@/public/images/icon-frontend-mentor.svg";
+import linkedin from "@/public/images/icon-linkedin.svg";
+import twitter from "@/public/images/icon-twitter.svg";
+
+export const socialsData = [
+  { name: "Github", link: "https://www.github.com", image: github },
+  {
+    name: "Frontend-Mentor",
+    link: "https://www.frontendmentor.io/home",
+    image: frontend,
+  },
+  { name: "Linkedin", link: "https://www.linkedin.com/", image: linkedin },
+  { name: "twitter", link: "https://x.com/home", image: twitter },
+];
